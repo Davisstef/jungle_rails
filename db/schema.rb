19fdc0ed6392e9124d0f0cef42b8d63ba3d0862e
Stefan Darrell Davis <stefan.davis@mail.utoronto.ca>
2022-01-12 18:56:47 +0000

    Completompleted User Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,14 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20160625062916) do
-
+ActiveRecord::Schema.define(version: 20191017190050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
   create_table "line_items", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
@@ -31,10 +27,8 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
   end
-
   add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
-
   create_table "orders", force: :cascade do |t|
     t.integer  "total_cents"
     t.datetime "created_at",       null: false
@@ -42,7 +36,6 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.string   "stripe_charge_id"
     t.string   "email"
   end
-
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -53,9 +46,16 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
   end
+   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
-  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
-
+   create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+     t.string   "last_name"
+     t.string   "email"
+     t.string   "password_digest"
+     t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
